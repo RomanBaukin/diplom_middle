bcf7e5d749ac93decf040bdffeb1003d66498008
Roman Baukin <rbaukin1984@gmail.com>
2021-01-13 16:54:49 +0400

    Настроил проект

---
+++ src/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+import burgerMenu from './modules/burgerMenu';
+
+// Меню-бургер
+burgerMenu();
---
+++ src/modules/burgerMenu.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const burgerMenu = () => {
+  console.log(1);
+};
+
+export default burgerMenu;
